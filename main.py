e7ff22e1f8f5e377c1fade93073903b9647060e0
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-04-18 14:28:22 -0500

    update cors policy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.request import router as request_router
 from routes.prediction import router as prediction_router
 from routes.user import router as user_router
@@ -8,3 +9,17 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(prediction_router)
 app.include_router(request_router)
+
+
+origins = [
+    "http://localhost:4200",  # tu frontend Angular
+    "http://127.0.0.1:4200",  # por si acaso
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*", 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
